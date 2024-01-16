b3e5f1d50a1f6e8d0d489df03ed507fa02175363
Ibrahim <abdulmumeenibroheem51@gmail.com>
2024-01-16 17:56:53 +0100

    BUG:/handled post request validation

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-
-import requests
 from django.views.generic import TemplateView
 import sys
 from subprocess import run,PIPE
 from django.core.files.storage import FileSystemStorage  
 from django.conf import settings
+from django.http import HttpResponse
 import os
 
 # from pages.Binarize import enhance
@@ -15,8 +14,8 @@ import os
 class HomePageView(TemplateView):
     template_name = "Import_page/Master_Work.html"
 
-class LandingPageView(TemplateView):
-    template_name = 'Analyze_page/landing.html'
+# class LandingPageView(TemplateView):
+#     template_name = 'Analyze_page/landing.html'
     
 # class ResultPageView(TemplateView):
 #     template_name = 'Results_page/Results.html'
@@ -26,43 +25,48 @@ def result(request):
 
 def external(request):
     
-    # inp = request.POST.get('param')
-    image = request.FILES['img']
-    fs = FileSystemStorage()
-    filename = fs.save(image.name,image)
-    fileurl = fs.open(filename)
-    templateurl = fs.url(filename)
-    print("raw url =>",filename)
-    print("full url =>",fileurl)
-    print("template url =>",templateurl)
-
-
-
-
-
-    # Binarize file image
-    # --------   Binarize -------
-    # image = run([sys.executable,"C:\\Users\\SetUp\\Desktop\\code\\pages\\django_project\\Binarize.py",str(fileurl),str(filename)],shell = False,stdout = PIPE)
+    if request.method == 'POST':
+        # inp = request.POST.get('param')
+        image = request.FILES['img']
+        fs = FileSystemStorage()
+        filename = fs.save(image.name,image)
+        fileurl = fs.open(filename)
+        templateurl = fs.url(filename)
+        print("raw url =>",filename)
+        print("full url =>",fileurl)
+        print("template url =>",templateurl)
     
 
-    image = run([sys.executable,
-    os.path.join(settings.BASE_DIR, 'Binarize.py'),str(fileurl),str(filename)],shell = False,stdout = PIPE)
+
+
+
+        # Binarize file image
+        # --------   Binarize -------
+        # image = run([sys.executable,  "C:\\Users\\SetUp\\Desktop\\code\\pages\\django_  project\\Binarize.py",str(fileurl),str    (filename)],shell = False,stdout = PIPE)
     
-    # segmentize file image
-    # --------   Segmentize -------
-    image = run([sys.executable,os.path.join(settings.BASE_DIR,'segmentize.py'),str(fileurl),str(filename)],shell = False,stdout = PIPE)
 
-    # Normalize file image
-    # --------   Normalize -------
-    image = run([sys.executable,os.path.join(settings.BASE_DIR,'Normalize.py'),str(fileurl),str(filename)],shell = False,stdout = PIPE)
-
-    # Oriental Filtering of finger
-    # -------- Oriental filter -----
+        image = run([sys.executable,
+        os.path.join(settings.BASE_DIR, 'Binarize.py'), str(fileurl),str(filename)],shell = False,   stdout = PIPE)
     
-    # Thinning  of finger
-    # --------  Thinning -------
+        # segmentize file image
+        # --------   Segmentize -------
+        image = run([sys.executable,os.path.join    (settings.BASE_DIR,'segmentize.py'),str (fileurl),str(filename)],shell = False,stdout =  PIPE)
 
-    #Features of Fnger
-    # --------  Features ------
+        # Normalize file image
+        # --------   Normalize -------
+        image = run([sys.executable,os.path.join    (settings.BASE_DIR,'Normalize.py'),str(fileurl),    str(filename)],shell = False,stdout = PIPE)
 
-    return render(request,'Analyze_page/landing.html')
+        # Oriental Filtering of finger
+        # -------- Oriental filter -----
+    
+        # Thinning  of finger
+        # --------  Thinning -------
+
+        #Features of Fnger
+        # --------  Features ------
+
+        return render(request,'Analyze_page/landing.html')
+
+    else:
+        return HttpResponse("<h1 style = 'color:red;text-align:center;font-family: Ubuntu' >Invalid method Paramter Used!!</h1>")
+    
