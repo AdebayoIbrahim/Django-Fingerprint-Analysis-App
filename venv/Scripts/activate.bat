2720f391a8ca4188486e91498f0a2c17dd3d3035
Ibrahim <abdulmumeenibroheem51@gmail.com>
2024-01-16 15:20:32 +0100

    BIG Update:project restruture...

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\SetUp\Desktop\code\venv
+set VIRTUAL_ENV=C:\Users\SetUp\Desktop\code\pages\django_project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
