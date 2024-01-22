13e60003cdc4ea05da20ef3ee1e46620002de477
SetUp <98415538+AdebayoIbrahim@users.noreply.github.com>
2024-01-22 08:51:24 +0100

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -44,11 +44,11 @@ def external(request):
     
         # segmentize file image
         # --------   Segmentize -------
-        image = run([sys.executable,os.path.join    (settings.BASE_DIR,'segmentize.py'),str (fileurl),str(filename)],shell = False,stdout =  PIPE)
+        # image = run([sys.executable,os.path.join    (settings.BASE_DIR,'segmentize.py'),str (fileurl),str(filename)],shell = False,stdout =  PIPE)
 
         # Normalize file image
         # --------   Normalize -------
-        image = run([sys.executable,os.path.join    (settings.BASE_DIR,'Normalize.py'),str(fileurl),    str(filename)],shell = False,stdout = PIPE)
+        # image = run([sys.executable,os.path.join    (settings.BASE_DIR,'Normalize.py'),str(fileurl),    str(filename)],shell = False,stdout = PIPE)
 
         # Oriental Filtering of finger
         # -------- Oriental filter -----
